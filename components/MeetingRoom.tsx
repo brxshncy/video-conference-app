4d64fd5c09dd149e04d2ca0541d889d19333ed89
brxshncy <ggkidsbruce619@gmail.com>
2024-06-23 20:43:31 +0800

    progress

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,40 +1,91 @@
-
 import { cn } from "@/lib/utils";
-import { SpeakerLayout, PaginatedGridLayout, CallParticipantsList, CallControls } from "@stream-io/video-react-sdk";
-import React, {useState} from "react";
+import {
+    SpeakerLayout,
+    PaginatedGridLayout,
+    CallParticipantsList,
+    CallControls,
+} from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
 
-type CallOutType = 'grid' | 'speaker-left' | 'speaker-right';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LayoutList } from "lucide-react";
+
+type CallLayOutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
-
-    const [layout, setLayout] = useState<CallOutType>('speaker-left');
-    const [showParticipants, setShowParticipants] = useState<boolean>(true)
+    const [layout, setLayout] = useState<CallLayOutType>("speaker-left");
+    const [showParticipants, setShowParticipants] = useState<boolean>(true);
 
     const CallLayout = () => {
         switch (layout) {
-            case 'grid':
-                    return <PaginatedGridLayout />
-            case 'speaker-right':
-                    return <SpeakerLayout participantsBarPosition={"left"} />
-            case 'speaker-left':
-                    return <SpeakerLayout participantsBarPosition={"right"} />
+            case "grid":
+                return <PaginatedGridLayout />;
+            case "speaker-right":
+                return <SpeakerLayout participantsBarPosition={"left"} />;
+            case "speaker-left":
+                return <SpeakerLayout participantsBarPosition={"right"} />;
             default:
-                return <PaginatedGridLayout />
+                return <PaginatedGridLayout />;
         }
-    }
-    return <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
-            <div className="relative flex size-full items-center justify-center">
-                <div className="flex size-full max-w-[1000px]">
+    };
+    return (
+        <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
+            <div className='relative flex size-full items-center justify-center'>
+                <div className='flex size-full max-w-[1000px]'>
                     <CallLayout />
                 </div>
-                <div className={cn("h-[calc(100vh-86px)] hidden ml-2", {'show-block': showParticipants})}>
-                    <CallParticipantsList onClose={() => setShowParticipants(false)}/>
+                <div
+                    className={cn("h-[calc(100vh-86px)] hidden ml-2", {
+                        "show-block": showParticipants,
+                    })}
+                >
+                    <CallParticipantsList
+                        onClose={() => setShowParticipants(false)}
+                    />
                 </div>
             </div>
-            <div className="fixed bottom-0 flex w-full items-center gap-5 justify-center" >
+            <div className='fixed bottom-0 flex w-full items-center gap-5 justify-center'>
                 <CallControls />
+                <DropdownMenu>
+                    <div className='flex items-center'>
+                        <DropdownMenuTrigger className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
+                            <LayoutList size={20} className='text-white' />
+                        </DropdownMenuTrigger>
+                    </div>
+
+                    <DropdownMenuContent className='border-dark-1 bg-dark-1 text-white'>
+                        {["Grid", "Speaker-Left", "Speaker-Right"].map(
+                            (item, index) => {
+                                return (
+                                    <div key={index}>
+                                        <DropdownMenuItem
+                                            className='cursor-pointer'
+                                            onClick={() =>
+                                                setLayout(
+                                                    item.toLowerCase() as CallLayOutType
+                                                )
+                                            }
+                                        >
+                                            {item}
+                                        </DropdownMenuItem>
+                                        <DropdownMenuSeparator className="border-dark-1"/>
+                                    </div>
+                                );
+                            }
+                        )}
+                
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
-    </section>;
+        </section>
+    );
 };
 
 export default MeetingRoom;
--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,20 +1,23 @@
-import { VideoPreview, useCall, DeviceSettings } from "@stream-io/video-react-sdk";
+import {
+    VideoPreview,
+    useCall,
+    DeviceSettings,
+} from "@stream-io/video-react-sdk";
 import React, { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 
 const MeetingSetup = ({
-    setIsSetuComplete
-}: {setIsSetuComplete: (value:boolean) => void}) => {
+    setIsSetuComplete,
+}: {
+    setIsSetuComplete: (value: boolean) => void;
+}) => {
     const [isMicCamToggledOn, setIsMicCamToggledOn] = useState<boolean>(false);
     const call = useCall();
 
-
     if (!call) {
-        throw new Error(
-            'usecall must be used within StreamCall component'
-        )
+        throw new Error("usecall must be used within StreamCall component");
     }
-    
+
     useEffect(() => {
         if (isMicCamToggledOn) {
             call?.camera.disable();
@@ -29,22 +32,24 @@ const MeetingSetup = ({
         <div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
             <h1 className='text-2xl font-bold'>Setup</h1>
             <VideoPreview />
-            <div className="flex h-16 items-center justify-center gap-3">
-                <label className="flex items-center justify-center gap-2 font-medium">
+            <div className='flex h-16 items-center justify-center gap-3'>
+                <label className='flex items-center justify-center gap-2 font-medium'>
                     <input
-                        type="checkbox" 
+                        type='checkbox'
                         checked={isMicCamToggledOn}
-                        onChange={e => setIsMicCamToggledOn(e.target.checked)}
-                            
+                        onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
                     />
                     Join with mic and camera off
                 </label>
                 <DeviceSettings />
             </div>
-            <Button className="rounded-md bg-green-500 px-4 py-2.5" onClick={() => {
-                call.join()
-                setIsSetuComplete(true)
-            }}>
+            <Button
+                className='rounded-md bg-green-500 px-4 py-2.5'
+                onClick={() => {
+                    call.join();
+                    setIsSetuComplete(true);
+                }}
+            >
                 Join Meeting
             </Button>
         </div>
